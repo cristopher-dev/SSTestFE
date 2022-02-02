9978a01ad68f66fef6139dcb654e85a6b2d04bed
cristopher <csmv_8@hotmail.com>
2022-02-02 12:07:31 -0500

    chore(component) add dropDownList

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { DropDownListComponent } from './drop-down-list/drop-down-list.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DropDownListComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })

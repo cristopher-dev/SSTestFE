6fa7d75be7633426bd2c84723ec91c29a2783e19
cristopher <csmv_8@hotmail.com>
2022-02-02 12:08:57 -0500

    chore(component-home) add kendoGrid and dropDown

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DropDownListComponent } from './drop-down-list/drop-down-list.component';
+import { KendoGridComponent } from './kendo-grid/kendo-grid.component';
 
 @NgModule({
-  declarations: [HomeComponent, DropDownListComponent],
+  declarations: [HomeComponent, DropDownListComponent, KendoGridComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })

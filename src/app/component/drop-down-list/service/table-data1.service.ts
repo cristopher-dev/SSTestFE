b2459d515b277f74de99e7e21e7f8a41f9d4687b
cristopher <csmv_8@hotmail.com>
2022-02-03 11:17:27 -0500

    chore(c-drop-down-list) add  http cliente

--- src/app/component/drop-down-list/service/table-data1.service.ts
+++ src/app/component/drop-down-list/service/table-data1.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TableData1Service {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  create() {}
+  findAll() {}
+  update() {}
+  remove() {}
 }
--- src/app/component/drop-down-list/service/table-data3.service.ts
+++ src/app/component/drop-down-list/service/table-data3.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TableData3Service {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  create() {}
+  findAll() {}
+  update() {}
+  remove() {}
 }

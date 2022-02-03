454bbc16d013dd005c34a7601cd2a5b3169225ab
cristopher <csmv_8@hotmail.com>
2022-02-03 10:49:21 -0500

    chore(c-home) add  title

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 import { HomeComponent } from './home/home.component';
 import { DropDownListComponent } from './drop-down-list/drop-down-list.component';
@@ -10,7 +12,14 @@ import { KendoGridComponent } from './kendo-grid/kendo-grid.component';
 
 @NgModule({
   declarations: [HomeComponent, DropDownListComponent, KendoGridComponent],
-  imports: [CommonModule, DropDownsModule, FormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    DropDownsModule,
+    FormsModule,
+    HttpClientModule,
+    GridModule,
+    ButtonsModule,
+  ],
   exports: [HomeComponent],
 })
 export class ComponentModule {}

909cb1c70cafa4509ab3cdc6768514f8d1ca4ed8
cristopher <csmv_8@hotmail.com>
2022-02-03 11:02:13 -0500

    chore(c-kendoGrid) add  table

--- src/app/component/kendo-grid/kendo-grid.component.ts
+++ src/app/component/kendo-grid/kendo-grid.component.ts
@@ -1,15 +1,85 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+
+const products = [
+  {
+    ProductID: 1,
+    ProductName: 'Chai',
+    SupplierID: 1,
+    CategoryID: 1,
+    QuantityPerUnit: '10 boxes x 20 bags',
+    UnitPrice: 18.0,
+    UnitsInStock: 39,
+    UnitsOnOrder: 0,
+    ReorderLevel: 10,
+    Discontinued: false,
+    Category: {
+      CategoryID: 1,
+      CategoryName: 'Beverages',
+      Description: 'Soft drinks, coffees, teas, beers, and ales',
+    },
+  },
+  {
+    ProductID: 2,
+    ProductName: 'Chang',
+    SupplierID: 1,
+    CategoryID: 1,
+    QuantityPerUnit: '24 - 12 oz bottles',
+    UnitPrice: 19.0,
+    UnitsInStock: 17,
+    UnitsOnOrder: 40,
+    ReorderLevel: 25,
+    Discontinued: false,
+    Category: {
+      CategoryID: 1,
+      CategoryName: 'Beverages',
+      Description: 'Soft drinks, coffees, teas, beers, and ales',
+    },
+  },
+  {
+    ProductID: 3,
+    ProductName: 'Aniseed Syrup',
+    SupplierID: 1,
+    CategoryID: 2,
+    QuantityPerUnit: '12 - 550 ml bottles',
+    UnitPrice: 10.0,
+    UnitsInStock: 13,
+    UnitsOnOrder: 70,
+    ReorderLevel: 25,
+    Discontinued: false,
+    Category: {
+      CategoryID: 2,
+      CategoryName: 'Condiments',
+      Description: 'Sweet and savory sauces, relishes, spreads, and seasonings',
+    },
+  },
+];
+
+class Product {
+  public ProductID: number = 0;
+  public ProductName = '';
+  public Discontinued = false;
+  public UnitsInStock: number = 0;
+  public UnitPrice = 0;
+}
 
 @Component({
   selector: 'app-kendo-grid',
   templateUrl: './kendo-grid.component.html',
-  styleUrls: ['./kendo-grid.component.css']
+  styleUrls: ['./kendo-grid.component.css'],
 })
-export class KendoGridComponent implements OnInit {
+export class KendoGridComponent {
+  public products: any[] = products;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  createNewProduct(): Product {
+    return new Product();
   }
 
+  saveHandler(e: any) {
+    console.log();
+  }
+  removeHandler(e: any) {
+    console.log();
+  }
 }

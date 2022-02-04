3b05772366d95bfe00dca20011f0eeb4361976db
cristopher <csmv_8@hotmail.com>
2022-02-04 13:37:44 -0500

    fix(c-grid): model

--- src/app/component/kendo-grid/model.ts
+++ src/app/component/kendo-grid/model.ts
@@ -1,10 +1,10 @@
 export class table {
   public id: number = 0;
-  public T1C2: number | string = 0;
-  public T1C4: number | string = 0;
-  public T1C3: number | string = '';
-  public T1C1: string | number = '';
-  public T2C1: number = 0;
+  public T1C1: number = 0;
+  public T1C2: string = '';
+  public T1C3: number = 0;
+  public T1C4: string = '';
+  public T2C1: string = '';
   public T2C2: string = '';
   public T2C3: number = 0;
   public T2C4: string = '';
